59766c5ceefdd53db727cbee59b281d8e5779eef
Christian Justo <cjusto@uabc.edu.mx>
2018-11-28 17:59:09 -0800

    maven

---
+++ src/test/java/UABC/test/tast.java
@@ -0,0 +1,15 @@
+package UABC.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class tast {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		assertEquals(4,4);
+	}
+
+}
